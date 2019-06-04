e1dca8dbf463e760e1d671a68bbdb5cf3a4dead7
Michael <print@3daddict.com>
2019-06-03 20:59:42 -0700

    Task model and item created

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,23 +6,39 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 });
 
-//Model Schema
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-    age: {
-        type: Number
-    }
+// //Model Schema
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String
+//     },
+//     age: {
+//         type: Number
+//     }
+// });
+
+// const me = new User({
+//     name: "Mike",
+//     age: "Andrew"
+// });
+
+// me.save().then(() => {
+//     console.log('Me!', me);
+// }).catch((error) => {
+//     console.log('Error!', error);
+// });
+
+const Task = mongoose.model('Task', {
+    description: String,
+    completed: Boolean
 });
 
-const me = new User({
-    name: "Mike",
-    age: "Andrew"
+const newTask = new Task({
+    description: "yakity yak don't talk back",
+    completed: false
 });
 
-me.save().then(() => {
-    console.log('Me!', me);
+newTask.save().then(() => {
+    console.log('Task:', newTask);
 }).catch((error) => {
     console.log('Error!', error);
-});
+})
