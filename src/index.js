cfda7caae4a35e850f5349159193b9169189ff00
Michael <print@3daddict.com>
2019-06-18 21:09:54 -0700

    delete task and user created

--- src/index.js
+++ src/index.js
@@ -70,6 +70,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if(!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
@@ -127,7 +141,21 @@ app.patch('/tasks/:id', async (req, res) => {
     } catch (e) {
         res.status(400).send(e)
     }
-})
+});
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if(!task) {
+            return res.status(404).send();
+        }
+        
+        res.send(task);
+    } catch (e) {
+        res.status(400).send(e);
+    }
+});
 
 app.listen(port, () => {
     console.log('Server is running on port', port);
