a1499271cb501f6d6e2ebe3d2954fb58edb8e921
Michael <print@3daddict.com>
2019-06-20 23:00:00 -0700

    authentication created for users

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //express middleware
-app.use((req, res, next) => {
-    if (req.method === 'GET') {
-        res.send('GET requests are disabled')
-    } else {
-        next();
-    }
-});
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next();
+//     }
+// });
 
 //parse json with express
 app.use(express.json());
