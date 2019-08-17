f75adf2c16d15a44de6e10e17a7fc379e2960323
Michael <print@3daddict.com>
2019-08-17 09:38:20 -0700

    sign up test case created

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,10 @@
 const request = require('supertest');
+const app = require('../src/app');
+
+test("Should sign-up new user", async () => {
+    await request(app).post('/users').send({
+        name: 'Mike',
+        email: 'mike@example.com',
+        password: 'MyPass777!'
+    });
+});
